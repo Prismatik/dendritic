4eb7a12c6001b829eace5c1e4bc988c9dda95aa4
David Banham <david@banham.id.au>
2015-12-07 06:38:56 +1100

    Fix npm binary

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 const path = require('path');
 const command = process.argv[2];
 
-const doer = require(path.resolve('.', command));
+const doer = require(path.resolve(__dirname, command));
 
 doer.apply(this, process.argv.slice(3));
