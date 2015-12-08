494a6f0836f1dc0bd23d1955accbf35c2102bb19
David Banham <david@banham.id.au>
2015-12-08 12:28:12 +1100

    Add SLOW_TEST option
    Normally, the tests scaffold the app into a child directory then runs
    the tests. This means that the scaffolded app uses the node_modules
    directory of the parent. That's why all the deps of a Claytons app are
    dev-deps of the Claytons project itself.
    
    This makes it faster, but can mask errors if you forgot to add
    something to package.mustache.
    
    The SLOW_TEST flag tells the test runner to do everything in a tmp dir
    and run npm install first. It takes forever, because npm, but it's more
    robust. Useful for CI environments and pre-release tests.

--- test.js
+++ test.js
@@ -2,19 +2,30 @@ const spawn = require('child_process').spawnSync;
 const bandname = require("bandname");
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
+const path = require('path');
+const os = require('os');
 
 const randomstring = () => bandname().replace(' ', '_').replace(/-/g, '_');
 
-const dir = './tests/'+randomstring();
+var dir;
+if (process.env.SLOW_TEST) {
+  dir = path.join(os.tmpdir(), 'claytons_tests', randomstring());
+} else {
+  dir = path.join(__dirname, 'tests', randomstring());
+}
+
 mkdirp.sync(dir);
 
 const opts = {cwd: dir, stdio: 'inherit'};
+const index = path.join(__dirname, 'index.js');
 
 const appName = 'claytons_test_'+randomstring();
 const controllerName = randomstring();
 
-spawn('node', ['../../index.js', 'base', appName, appName+'s'], opts);
-spawn('node', ['../../index.js', 'controller', controllerName, controllerName+'s'], opts);
+
+spawn('node', [index, 'base', appName, appName+'s'], opts);
+spawn('node', [index, 'controller', controllerName, controllerName+'s'], opts);
+if (process.env.SLOW_TEST) spawn('npm', ['install'], opts);
 spawn('npm', ['test'], opts);
 
 rimraf.sync(dir);
