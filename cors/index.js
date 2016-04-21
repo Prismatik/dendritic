d558f25c071b418db224d0e0fd9006a025f31ab1
Simon Taylor <nizmox@gmail.com>
2016-04-21 13:38:47 +1000

    update cors to use scaffold

--- cors/index.js
+++ cors/index.js
@@ -1,26 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const mkdirp = require('mkdirp');
-const mustache = require('mustache');
 const _ = require('lodash');
+const scaffold = require('../lib/scaffold');
 
 module.exports = () => {
-  const things = [
-    {n: 'cors', p: 'tests/middleware'},
-    {n: 'cors', p: 'middleware'}
-  ].map(thing => _.extend({p: '', e: 'js'}, thing));
+  const files = [
+    {p: 'middleware', n: 'cors'},
+    {p: 'tests/middleware', n: 'cors'}
+  ].map(thing => _.extend({p: '', d: thing.p || '', t: thing.n, e: 'js'}, thing));
 
-  things.forEach(thing => {
-    const templatePath = path.join(__dirname, thing.p, thing.n+'.mustache');
-    const template = fs.readFileSync(templatePath).toString();
-    const dir = path.join(process.cwd(), thing.p);
-    const target = path.join(dir, thing.n+'.'+thing.e);
-    console.log('writing', thing.n, 'to', target);
-    if (fs.existsSync(target)) return console.error(target, 'already exists. Not overwriting it');
-
-    mkdirp.sync(dir);
-    const rendered = mustache.render(template);
-    fs.writeFileSync(target, rendered);
-  });
-
+  files.forEach(scaffold({}, __dirname));
 };
