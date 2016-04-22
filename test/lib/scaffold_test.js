45830cda7bfee7902b5853c126125d17a7e1544c
Simon Taylor <nizmox@gmail.com>
2016-04-22 16:28:16 +1000

    swap double quotes for quotes

--- test/lib/scaffold_test.js
+++ test/lib/scaffold_test.js
@@ -1,21 +1,21 @@
-const fs = require("fs");
-const rimraf = require("rimraf");
-const scaffold = require("../../lib/scaffold");
+const fs = require('fs');
+const rimraf = require('rimraf');
+const scaffold = require('../../lib/scaffold');
 
-const sourcePath = process.cwd() + "/test/files/lib";
-const outputFolder = "/redbeard_tests/redbeard";
-const outputPath = process.cwd() + "/redbeard_tests/redbeard";
+const sourcePath = process.cwd() + '/test/files/lib';
+const outputFolder = '/redbeard_tests/redbeard';
+const outputPath = process.cwd() + '/redbeard_tests/redbeard';
 
-describe("lib/scaffold", function() {
-  describe(".scaffold", function() {
+describe('lib/scaffold', function() {
+  describe('.scaffold', function() {
     afterEach(function() {
       rimraf.sync(outputPath);
     });
 
-    it("must create destination folder", function() {
+    it('must create destination folder', function() {
       scaffold({
         basePath: sourcePath,
-        files: [{n: "scaffold", d: outputFolder}],
+        files: [{n: 'scaffold', d: outputFolder}],
         mustacheOpts: {}
       });
       
@@ -23,64 +23,64 @@ describe("lib/scaffold", function() {
       folder.isDirectory().must.be.true();
     });
 
-    it("must create target file", function() {
+    it('must create target file', function() {
       scaffold({
         basePath: sourcePath,
-        files: [{n: "scaffold", d: outputFolder}],
+        files: [{n: 'scaffold', d: outputFolder}],
         mustacheOpts: {}
       });
       
-      const folder = fs.lstatSync(outputPath + "/scaffold.js");
+      const folder = fs.lstatSync(outputPath + '/scaffold.js');
       folder.isDirectory().must.be.false();
     });
 
-    it("must allow specifying the target file name", function() {
+    it('must allow specifying the target file name', function() {
       scaffold({
         basePath: sourcePath,
-        files: [{n: "scaffold", d: outputFolder, t: "yellowbeard"}],
+        files: [{n: 'scaffold', d: outputFolder, t: 'yellowbeard'}],
         mustacheOpts: {}
       });
       
-      const folder = fs.lstatSync(outputPath + "/yellowbeard.js");
+      const folder = fs.lstatSync(outputPath + '/yellowbeard.js');
       folder.isDirectory().must.be.false();
     });
 
-    it("must allow specifying the file extension", function() {
+    it('must allow specifying the file extension', function() {
       scaffold({
         basePath: sourcePath,
-        files: [{n: "scaffold", d: outputFolder, e: "html"}],
+        files: [{n: 'scaffold', d: outputFolder, e: 'html'}],
         mustacheOpts: {}
       });
       
-      const folder = fs.lstatSync(outputPath + "/scaffold.html");
+      const folder = fs.lstatSync(outputPath + '/scaffold.html');
       folder.isDirectory().must.be.false();
     });
 
-    it("must allow specifying the files path", function() {
+    it('must allow specifying the files path', function() {
       scaffold({
         basePath: process.cwd(),
-        files: [{p: "test/files/lib", n: "scaffold", d: outputFolder}],
+        files: [{p: 'test/files/lib', n: 'scaffold', d: outputFolder}],
         mustacheOpts: {}
       });
       
-      const folder = fs.lstatSync(outputPath + "/scaffold.js");
+      const folder = fs.lstatSync(outputPath + '/scaffold.js');
       folder.isDirectory().must.be.false();
     });
 
-    it("must render .mustache template", function() {
+    it('must render .mustache template', function() {
       const mustacheOpts = {
-        header: "Colors",
+        header: 'Colors',
         items: [
-            {name: "red", first: true, url: "#Red"},
-            {name: "green", link: true, url: "#Green"},
-            {name: "blue", link: true, url: "#Blue"}
+            {name: 'red', first: true, url: '#Red'},
+            {name: 'green', link: true, url: '#Green'},
+            {name: 'blue', link: true, url: '#Blue'}
         ],
         empty: false
       };
 
       scaffold({
         basePath: sourcePath,
-        files: [{n: "scaffold", d: outputFolder}],
+        files: [{n: 'scaffold', d: outputFolder}],
         mustacheOpts: mustacheOpts
       });
 
@@ -90,8 +90,8 @@ describe("lib/scaffold", function() {
       expected += '    <li><a href="#Green">green</a></li>\n';
       expected += '    <li><a href="#Blue">blue</a></li>\n';
       expected += '\n';
-      
-      const file = fs.readFileSync(outputPath + "/scaffold.js", "utf-8");
+
+      const file = fs.readFileSync(outputPath + '/scaffold.js', 'utf-8');
       file.must.equal(expected);
     });
   });
