ec90b89b0b578c5d6e09f22634442e60cc0815e9
Simon Taylor <nizmox@gmail.com>
2016-04-29 10:03:32 +1000

    rename user.mustache to schema.mustache

--- user/index.js
+++ user/index.js
@@ -4,7 +4,7 @@ module.exports = (opts) => {
   // p: source path, n: filename
   // d: destination path, t: target filename, e: target extension
   const files = [
-    {p: 'schemas', n: 'user', e: 'json'}
+    {p: 'schemas', n: 'schema', t: opts.name, e: 'json'}
   ];
 
   scaffold({ basePath: __dirname, files: files, mustacheOpts: opts });
