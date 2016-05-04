dca78109a6e5ddaa94a7cdb61966ad19df80ea3d
Simon Taylor <nizmox@gmail.com>
2016-05-04 16:24:17 +1000

    add multi relationships

--- test.js
+++ test.js
@@ -22,11 +22,13 @@ const index = path.join(__dirname, 'index.js');
 const appName = 'redbeard_tests'+randomstring();
 const controllerName1 = randomstring();
 const controllerName2 = randomstring();
+const controllerName3 = randomstring();
 
 exec(['node', index, 'base', appName].join(' '), opts);
 if (process.env.SLOW_TEST) exec(['npm', 'install'].join(' '), opts);
 exec(['node', index, 'controller', controllerName1].join(' '), opts);
-exec(['node', index, 'controller', controllerName2, '-r', controllerName1].join(' '), opts);
+exec(['node', index, 'controller', controllerName2, '-s', controllerName1].join(' '), opts);
+exec(['node', index, 'controller', controllerName3, '-m', controllerName2].join(' '), opts);
 exec(['node', index, 'cors'].join(' '), opts);
 exec(['npm', 'test'].join(' '), opts);
 
