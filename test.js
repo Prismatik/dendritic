dfc523496065c817b0a1156eb75eec1058835a4e
Simon Taylor <nizmox@gmail.com>
2016-05-12 09:30:16 +1000

    move linting to after mocha tests

--- test.js
+++ test.js
@@ -31,9 +31,8 @@ exec(['node', index, 'controller', controllerName2, '-s', controllerName1].join(
 exec(['node', index, 'user', userName, '-m', controllerName2].join(' '), opts);
 if (process.env.SLOW_TEST) exec(['npm', 'install'].join(' '), opts);
 exec(['cp', 'example.env', '.env'].join(' '), opts);
-try { exec(['./node_modules/.bin/eslint', './redbeard_tests/'].join(' '), _.omit(opts, 'cwd')); }
-catch (e) { console.error("Eslint errored: please review lint errors above"); }
 exec(['npm', 'test'].join(' '), opts);
 exec(['npm', 'run', 'testredbeard'].join(' '), _.omit(opts, 'cwd'));
+exec(['./node_modules/.bin/eslint', './redbeard_tests/'].join(' '), _.omit(opts, 'cwd'));
 
 rimraf.sync(dir);
