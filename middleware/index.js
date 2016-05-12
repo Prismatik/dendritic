44f581be5336dc72b9bc98f3989db086f79187d4
Nathan Winch <nwinch@gmail.com>
2016-05-12 17:16:47 +1000

    Add middleware generator

---
+++ middleware/index.js
@@ -0,0 +1,9 @@
+const scaffold = require('../lib/scaffold');
+
+module.exports = (opts) => {
+  const files = [
+    {p: 'middleware', n: 'template', t: opts.name}
+  ];
+
+  scaffold({ basePath: __dirname, files: files, mustacheOpts: opts });
+};
