cf8c948207fd2e888ba4486047a3a37328cc6775
David Banham <david@banham.id.au>
2016-06-03 17:27:53 +1000

    Add captains log

---
+++ test/lib/captains_log_test.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const rimraf = require('rimraf');
+const captainsLog = require('../../lib/captains_log');
+
+const root = process.cwd();
+const outputPath = `${root}/redbeard_tests/`;
+const logPath = `${root}/redbeard_tests/captains_log`;
+
+describe.only('lib/captainsLog', () => {
+  let origwd = '';
+  before(() => {
+    origwd = process.cwd();
+    process.chdir(outputPath);
+  });
+  afterEach(() => {
+    rimraf.sync(logPath);
+  });
+  after(() => {
+    process.chdir(origwd);
+  });
+
+  it('must create captains_log', () => {
+    fs.existsSync(logPath).must.be.false();
+    captainsLog(['node', 'redbeard', 'what', 'up']);
+    fs.existsSync(logPath).must.be.true();
+  });
+
+  it('must add the correct line to the log', () => {
+    fs.existsSync(logPath).must.be.false();
+    captainsLog(['node', 'redbeard', 'what', 'up', '-w', 'somearg']);
+    fs.readFileSync(logPath).toString().must.equal('redbeard what up -w somearg\n');
+  });
+});
